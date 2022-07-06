ed110f41c452dbbdd6b4f765c3ee54f98ed8e64a
BaianorASR <reis.adson2@outlook.com>
2022-07-06 16:27:43 -0300

    📦 ci: move repository to fork

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,50 @@
 import type { NextPage } from 'next';
 
-// import { vehiclesApi } from '../services/api';
+import { vehiclesApi } from '../services/api';
 
 const Home: NextPage = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <div>
+        <label htmlFor="search">
+          Search:
+          <input type="text" id="search" />
+          <button type="button">Search</button>
+        </label>
+        <button type="button">filtro</button>
+      </div>
+
+      <button type="button">Adicionar</button>
+
+      <section>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+          <li>d</li>
+          <li>e</li>
+          <li>f</li>
+          <li>g</li>
+          <li>h</li>
+          <li>i</li>
+          <li>j</li>
+        </ul>
+      </section>
+
+      <footer>create by baianim</footer>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const vehicles = await vehiclesApi.getAllVehicles();
+  return {
+    props: {
+      vehicles,
+    },
+  };
 };
 
 export default Home;
